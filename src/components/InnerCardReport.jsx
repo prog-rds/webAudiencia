748167992e239a0fa3f45ada1acd6015556fe646
Fernando Garnica <janyo1202@gmail.com>
2024-08-08 09:45:49 -0500

    component publicidad

--- src/components/InnerCardReport.jsx
+++ src/components/InnerCardReport.jsx
@@ -1,41 +1,35 @@
 const InnerCardReport = ({ userStudy, ads, interactions }) => {
 	return (
-		<div className='grid'>
-
-			<div className={`grid grid-cols-${ads.length}`}>
-				{
-					ads.map((a, i) => (
-						<span key={i} className='p-2 border border-black text-center'>Publicidad{` ${i + 1}`}</span>
-					))
-				}
-			</div>
-			<div className='p-2 text-center border border-black'>
+		<div className={`grid grid-rows-6 grid-cols-${ads.length}`}>
+			{
+				ads.map((a, i) => (
+					<div key={i} className={`p-2 border border-black text-center col-start-${i + 1} col-end-${i + 2}`}>Publicidad{` ${i + 1}`}</div>
+				))
+			}
+			<div className={`p-2 text-center border border-black col-start-1 col-end-${ads.length + 1}  col-span-${ads.length}`}>
 				{userStudy.StudyDate}
 			</div>
-			<div className='p-2 text-center border border-black'>
+			<div className={`p-2 text-center border border-black col-start-1 col-end-${ads.length + 1} col-span-${ads.length}`}>
 				{userStudy.StudyTime}
 			</div>
-			<div className={`grid grid-cols-${ads.length}`}>
-				{
-					interactions.map((i, j) => (
-						<span key={j} className='p-2 border border-black text-center'>{i.ViewTime}</span>
-					))
-				}
-			</div>
-			<div className={`grid grid-cols-${ads.length}`}>
-				{
-					ads.map((a, i) => (
-						<span key={i} className='p-2 border border-black text-center'>{a.AdEntryTime}</span>
-					))
-				}
-			</div>
-			<div className={`grid grid-cols-${ads.length}`}>
-				{
-					interactions.map((i, j) => (
-						<span key={j} className='p-2 border border-black text-center'>{i.WasSkipped}</span>
-					))
-				}
-			</div>
+			{
+				interactions.map((i, j) => (
+					<div key={j} className={`p-2 border border-black text-center col-start-${i + 1} col-end-${i + 2}`}>{i.ViewTime}</div>
+				))
+			}
+
+			{
+				ads.map((a, i) => (
+					<span key={i} className={`p-2 border border-black text-center col-start-${i + 1} col-end-${i + 2}`}>{a.AdEntryTime}</span>
+				))
+			}
+
+			{
+				interactions.map((i, j) => (
+					<span key={j} className={`p-2 border border-black text-center col-start-${i + 1} col-end-${i + 2}`}>{i.WasSkipped}</span>
+				))
+			}
+
 		</div>
 	);
 };
